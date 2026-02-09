00473515f4bb4de6332a8172c6c11807c9715d48
Sruthinila D <dsruthinila@kaartech.com>
2026-02-09 16:37:32 +0530

    repo folder added

---
+++ repo/Database.sql
@@ -0,0 +1,19 @@
+-- Create database
+CREATE DATABASE policy_db;
+
+-- Use database
+\c policy_db;
+
+-- =========================================
+-- Table: documents
+-- =========================================
+CREATE TABLE documents (
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  file_path TEXT NOT NULL,
+  file_type TEXT,
+  file_size INT,
+  status TEXT CHECK (status IN ('pending','approved','rejected')) DEFAULT 'pending',
+  created_at TIMESTAMP DEFAULT now(),
+  reviewed_at TIMESTAMP
+);
